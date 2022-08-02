c7a4498f96cf12a0eb30780f33ddc87764b94bc4
Hiruthik J <56921861+hiruthikj@users.noreply.github.com>
2022-08-02 08:34:31 +0530

    Fix

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,11 +3,12 @@
 # Install Oh My ZSH
 
 # Check for Oh My ZSH
-if [ -d ~/.oh-my-zsh ]; then
+if ! [ -d ~/.oh-my-zsh ]; then
 	echo "  Installing oh-my-zsh for you."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
  else
- 	echo "oh-my-zsh is not installed"
+ 	echo "Oh-my-zsh is installed with p10k theme"
 fi
 
 
